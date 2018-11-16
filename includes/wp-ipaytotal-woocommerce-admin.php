b803e40a6c57392761f85d72fde5f047bdfc46af
iPayTotal Ltd <43794351+ipaytotaltd@users.noreply.github.com>
2018-11-16 17:38:54 +0530

    Update wp-ipaytotal-woocommerce-admin.php

--- includes/wp-ipaytotal-woocommerce-admin.php
+++ includes/wp-ipaytotal-woocommerce-admin.php
@@ -203,7 +203,7 @@ class wowp_iptwpg_ipaytotal extends WC_Payment_Gateway_CC {
 			wc_add_notice( __('Payment successful. ') . $response_body['message'] . ' - ' . $response_body['descripcion'] . '.', 'error' );
 
 			 //Redirect to thank you page
-			 //return array( 'result'   => 'success','redirect' => $this->get_return_url( $customer_order ) );
+			 return array( 'result'   => 'success','redirect' => $this->get_return_url( $customer_order ) );
 		} 
 		
 		//else 
